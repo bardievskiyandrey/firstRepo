7cb8e9c8cbe15a25afede73799c4327a8e76c1cf
bardievskiyandrey <andrulikbardievskiy@gmail.com>
2024-11-11 16:23:30 +0200

    class

--- 11-11-24/classwork.py
+++ 11-11-24/classwork.py
@@ -1,5 +1,5 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", encoding="UTF-8")
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", encoding="UTF-8", format="We have next log message: %(levelname)s:%(asctime)s - &(message)s")
 logging.debug("Debug message ававав")
 logging.info("Info message")
 logging.warning("warning message")
