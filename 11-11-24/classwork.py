8611c774a37a2065ab9b6bc825318f5b4436b55f
bardievskiyandrey <andrulikbardievskiy@gmail.com>
2024-11-11 16:06:35 +0200

    dz oshibki

---
+++ 11-11-24/classwork.py
@@ -0,0 +1,7 @@
+import logging
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", encoding="UTF-8")
+logging.debug("Debug message ававав")
+logging.info("Info message")
+logging.warning("warning message")
+logging.error("error message ")
+logging.critical("critical message ")
