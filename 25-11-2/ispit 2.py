ee6ab149c58cce57e3f8c8093f3c436db6c2e6e6
bardievskiyandrey <andrulikbardievskiy@gmail.com>
2024-11-25 17:33:39 +0200

    ispitt

--- 25-11-2/ispit 2.py
+++ 25-11-2/ispit 2.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 response = requests.get("https://www.quotegarden.com/mind.html")
-soup = BeautifulSoup(response.text, features="html.parser")
-text_block = soup.find_all("font")
+soup = BeautifulSoup(response.content, features="html.parser")
+text_block = soup.find_all("p")
 print(text_block)
